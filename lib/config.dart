f8f5cfa09859272c81628d7c5ac77b3473b245ed
Mory Keita <codewithmory@gmail.com>
2020-08-10 03:22:43 -0400

    fixing bug

--- lib/config.dart
+++ lib/config.dart
@@ -1,7 +1,14 @@
 class Config {
   final String url;
-  final Map<String, String> headers;
   final int pingInterval;
+  final Map<String, String> stompConnectHeaders;
 
-  Config({this.url, this.headers, this.pingInterval});
+  /// Headers to be passed when connecting to WebSocket
+  final Map<String, dynamic> webSocketConnectHeaders;
+
+  Config(
+      {this.url,
+      this.stompConnectHeaders,
+      this.pingInterval,
+      this.webSocketConnectHeaders});
 }
